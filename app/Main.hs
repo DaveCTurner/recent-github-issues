478076caf027a769ba91526d8e6470e18feb0d8c
David Turner <david.turner@elastic.co>
2020-02-06 15:14:14 +0000

    Fix abbrs

--- app/Main.hs
+++ app/Main.hs
@@ -51,7 +51,8 @@ issueRepoAbbreviation i = if
   | repo == "https://api.github.com/repos/elastic/elasticsearch"                  -> ""
   | repo == "https://api.github.com/repos/elastic/support-support-help"           -> "ssh"
   | repo == "https://api.github.com/repos/elastic/support-dev-help"               -> "sdh"
-  | repo == "https://api.github.com/repos/elastic/support-dev-help-elasticsearch" -> "sdhe"
+  | repo == "https://api.github.com/repos/elastic/sdh-elasticsearch"              -> "sdhe"
+  | repo == "https://api.github.com/repos/elastic/cloud-on-k8s"                   -> "eck"
   | "https://api.github.com/repos/elastic/" `T.isPrefixOf` repo                   -> T.unpack $ T.drop (T.length "https://api.github.com/repos/elastic/") repo
   | "https://api.github.com/repos/"         `T.isPrefixOf` repo                   -> T.unpack $ T.drop (T.length "https://api.github.com/repos/")         repo
   | otherwise                                                                     -> error $ "unknown repo format: " ++ show repo
